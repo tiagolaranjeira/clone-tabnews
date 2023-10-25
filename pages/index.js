8efc17a0c000a130e6bd713c7af7eb0904836909
Tiago Laranjeira <102994515+tiagolaranjeira@users.noreply.github.com>
2023-10-25 15:43:05 +0000

    change_conteiner2

--- pages/index.js
+++ pages/index.js
@@ -8,28 +8,36 @@ function Home() {
       background: '#f0f0f0', // Cor de fundo do contêiner principal
     };
   
-    const containerStyles = [
-      {
-        background: '#FF5733', // Cor de fundo do primeiro contêiner
-      },
-      {
-        background: '#33FF57', // Cor de fundo do segundo contêiner
-      },
-      {
-        background: '#5733FF', // Cor de fundo do terceiro contêiner
-      },
-    ];
+    const containerStyle = {
+      flex: '1', // Ocupa todo o espaço disponível
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  
+    const container1Style = {
+      background: '#FF5733', // Cor de fundo do primeiro contêiner
+    };
+  
+    const container2Style = {
+      background: '#33FF57', // Cor de fundo do segundo contêiner
+    };
+  
+    const container3Style = {
+      background: '#5733FF', // Cor de fundo do terceiro contêiner
+    };
   
     return (
       <div style={mainContainerStyle}>
-        <div style={containerStyles[0]}>
+        <div style={{ ...containerStyle, ...container1Style }}>
           <h1>Vocês vão ter que me engolir!</h1>
         </div>
-        <div style={containerStyles[1]}>
-        <h1>Vocês vão ter que me engolir!</h1>
+        <div style={{ ...containerStyle, ...container2Style }}>
+          <p>Conteúdo do segundo contêiner</p>
         </div>
-        <div style={containerStyles[2]}>
-        <h1>Vocês vão ter que me engolir!</h1>
+        <div style={{ ...containerStyle, ...container3Style }}>
+          <p>Conteúdo do terceiro contêiner</p>
         </div>
       </div>
     );
