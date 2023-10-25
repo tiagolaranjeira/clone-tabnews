ecb8daace8cf44e23ba52af03c8487667e04cbd5
Tiago Laranjeira <102994515+tiagolaranjeira@users.noreply.github.com>
2023-10-25 15:13:57 +0000

    insert_container

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 function Home() {
-    const centerContainerStyle = {
+    const containerStyle = {
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      height: '100vh', // Isso define a altura da div para ocupar a altura inteira da tela
+      height: '100vh',
+      background: '#f0f0f0', // Cor de fundo
     };
   
     const centerTextStyle = {
       textAlign: 'center',
+      margin: '20px', // Margem para separar o texto do contêiner abaixo
     };
   
     return (
-      <div style={centerContainerStyle}>
+      <div style={containerStyle}>
         <h1 style={centerTextStyle}>Vocês vão ter que me engolir!</h1>
+        <div style={centerTextStyle}>Outro texto aqui</div>
+        <div style={centerTextStyle}>Mais texto</div>
       </div>
     );
   }
   
   export default Home;
+  
+  
+  
+  
