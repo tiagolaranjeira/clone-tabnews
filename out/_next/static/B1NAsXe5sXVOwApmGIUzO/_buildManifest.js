cd4377591144ed447c5a1db72fa6860bdc19bab3
Tiago Laranjeira <102994515+tiagolaranjeira@users.noreply.github.com>
2023-11-18 00:52:18 +0000

    teste configuracao

--- out/_next/static/B1NAsXe5sXVOwApmGIUzO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-05a4d87e35b130ef.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
